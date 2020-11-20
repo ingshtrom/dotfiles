2fa870bfbd3514a62e4d19e13aba442d58d50905
Alex Hokanson <571756+ingshtrom@users.noreply.github.com>
2020-11-20 08:53:39 -0500

    tmux upgrade, cooler stuff when runnign dotfiles
    Signed-off-by: Alex Hokanson <571756+ingshtrom@users.noreply.github.com>

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,30 +1,36 @@
-# Sets reasonable macOS defaults.
-#
+#Sets reasonable macOS defaults.
 
-# echo 'Setting reasonable defaults for OSX...'
 
-# # Disable press-and-hold for keys in favor of key repeat.
-# defaults write -g ApplePressAndHoldEnabled -bool true
+echo 'Setting reasonable defaults for OSX...'
 
-# # Use AirDrop over every interface. srsly this should be a default.
-# defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool true
 
-# # Always open everything in Finder's list view. This is important.
-# defaults write com.apple.Finder FXPreferredViewStyle Nlsv
-# defaults write com.apple.finder AppleShowAllFiles YES
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# # Show all hidden files and directories
-# chflags -R nohidden $HOME 2> /dev/null
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+defaults write com.apple.finder AppleShowAllFiles YES
 
-# # Set a really fast key repeat.
-# defaults write NSGlobalDomain KeyRepeat -int 1
+# Show all hidden files and directories
+chflags -R nohidden $HOME 2> /dev/null
 
-# # Set the Finder prefs for showing a few different volumes on the Desktop.
-# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-# defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
 
-# # disable press-and-hold for VSCode
-# defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool true
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# # faster mouse movement
-# # defaults write -g com.apple.mouse.scaling 6
+# disable press-and-hold for VSCode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool true
+
+# faster mouse movement
+# NOTE don't need it with logitech since the scaling
+#      is done in the app
+# defaults write -g com.apple.mouse.scaling 6
+
+# make sure indexing of spotlight is off
+# REF: https://osxdaily.com/2009/09/20/disable-spotlight-in-mac-os-x-10-6-snow-leopard/
+sudo mdutil -a -i off
